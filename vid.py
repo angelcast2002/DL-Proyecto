fe91c756ecd10f5d533a7be1f12d272fdf10b74a
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2024-11-07 22:31:14 -0600

    update
    Co-Authored-By: Diego Morales <77738746+Diego2250@users.noreply.github.com>
    Co-Authored-By: Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>

---
+++ vid.py
@@ -0,0 +1,25 @@
+import os
+import ffmpeg
+import shutil
+
+def extract_frames(video, output_folder):
+
+    input_path = f"/datavid/input/{video}"
+    out_path = f"/datavid/output/{output_folder}"
+
+    # Borrar el contenido de la carpeta de salida si existe
+    if os.path.exists(out_path):
+        shutil.rmtree(out_path)
+    os.makedirs(out_path, exist_ok=True)
+
+    # Usar ffmpeg para extraer fotogramas con soporte de GPU
+    (
+        ffmpeg
+        .input(input_path, hwaccel='cuda')  # Aceleración con CUDA
+        .output(os.path.join(out_path, 'frame_%04d.jpg'), start_number=0)
+        .run()
+    )
+
+    print("Extracción de fotogramas completada con aceleración GPU del video ", video_path) 
+    return out_path
+
